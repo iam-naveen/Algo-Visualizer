7b472e3d5710a20f3a4cec668889a222276fd9f6
Naveen <imnaveenbharath@gmail.com>
2023-06-04 23:14:18 +0530

    Vercel Changes 0.4

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.sendFile("/index.html");
+  res.sendFile("public/index.html");
 });
 
 app.listen(process.env.PORT || 3000, function () {
